9c930883037ce2c2007d537923de084d8808fc76
ValB <valvatorezbraveheart@gmail.com>
2025-03-29 15:14:07 -0400

    Edited AdminServiceTest to use jdbcTemplate, in a test table rather than the working table

--- messagingApp-backend/src/test/java/com/messagingApp/messagingApp_backend/services/AdminServiceTest.java
+++ messagingApp-backend/src/test/java/com/messagingApp/messagingApp_backend/services/AdminServiceTest.java
@@ -1,190 +1,134 @@
 package com.messagingApp.messagingApp_backend.services;
 
-import com.messagingApp.messagingApp_backend.models.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AdminServiceTest {
+@SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class AdminServiceTest {
 
+    @Autowired
     ChannelService channelService = new ChannelService();
+
+    @Autowired
     AdminService adminService = new AdminService(channelService);
 
+    private final String sampleMemberName = "member";
+    private final String sampleAdminName = "admin";
     String adminName = "admin";
-    String adminPassword = "admin";
-    String adminRole = "ADMIN";
-    String insertUser = "INSERT INTO users(username, password, role) VALUES (?, ?, ?)";
-    String deleteUser = "DELETE FROM users WHERE username = ?";
-    String insertMessage = "INSERT INTO messages(username, text, channel_name) VALUES (?, ?, ?)";
-    String getMessageId = "SELECT id FROM messages WHERE username = ? AND text = ? AND channel_name = ?";
-    String deleteMessage = "DELETE FROM messages WHERE username = ? AND text = ? AND channel_name = ?";
+    String getMessageId = "SELECT id FROM messages WHERE username = ? AND id = ?";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
-    void setUp() {
-        // Insert an admin to be used to be checked
-        channelService.executeUpdate(insertUser,"Error inserting user", adminName, adminPassword, adminRole);
+    public void setupDatabase() {
+        // Insert Member User
+        jdbcTemplate.execute("INSERT INTO users(username, password, role) VALUES ('member', 'member', 'MEMBER')");
+        // Insert Admin User
+        jdbcTemplate.execute("INSERT INTO users(username, password, role) VALUES ('admin', 'admin', 'ADMIN')");
+        // Inserting channel
+        jdbcTemplate.execute("INSERT INTO channels (name, type) VALUES ('General', 'PC')");
+        // Inserting user into channel
+        jdbcTemplate.execute("INSERT INTO user_channel (username, channel_name) VALUES ('member', 'General')");
+        jdbcTemplate.execute("INSERT INTO user_channel (username, channel_name) VALUES ('admin', 'General')");
+        // Inserting sample message
+        jdbcTemplate.execute("INSERT INTO public.messages (id, username, text, date_time, channel_name) VALUES (1, 'member', 'Remember to stay hydrated', '2025-03-29 14:05:23.254236', 'General')");
     }
 
     @AfterEach
-    void tearDown() {
-        channelService.executeUpdate(deleteUser, "Error in deleting user", adminName);
+    public void cleanupDatabase() {
+        jdbcTemplate.execute("TRUNCATE TABLE users, channels, messages RESTART IDENTITY CASCADE;");
     }
-
 
     @Test
     void isAdminReturnTrueForAdmin() {
-        String sampleAdminName = "SampleAdmin";
-        try {
-            // Insert Admin User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleAdminName, "empty", "ADMIN");
-            assertTrue(adminService.isAdmin(sampleAdminName), "Admin user should return true");
-
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleAdminName);
-        }
+        // Using the sample admin from the @BeforeEach
+        assertTrue(adminService.isAdmin(sampleAdminName), "Admin user should return true");
     }
+
     @Test
     void isAdminReturnFalseForMember() {
-        String sampleMemberName = "SampleMember";
-        try {
-            // Insert Member User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleMemberName, "empty", "MEMBER");
-            assertFalse(adminService.isAdmin(sampleMemberName), "Member user should return false");
-
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleMemberName);
-        }
-    }
-    @Test
-    void isAdminReturnFalseForNonExistingUser() {
-        String sampleMemberName = "SampleMember";
-        // Just assert, do not insert user
         assertFalse(adminService.isAdmin(sampleMemberName), "Member user should return false");
     }
 
     @Test
-    void updateUserRoleFromAdminToAdmin() {
-        String sampleUser = "SampleUser";
-        try {
-            // Insert Member User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleUser, "empty", "ADMIN");
-            assertTrue(adminService.isAdmin(sampleUser), "SampleUser is currently admin");
-            // Change to Admin
-            adminService.updateUserRole(adminName, sampleUser, "ADMIN");
-            assertTrue(adminService.isAdmin(sampleUser), "SampleUser is still admin");
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleUser);
-        }
-    }
-    @Test
-    void updateUserRoleFromAdminToMember() {
-        String sampleUser = "SampleUser";
-        try {
-            // Insert Admin User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleUser, "empty", "ADMIN");
-            assertTrue(adminService.isAdmin(sampleUser), "SampleUser is currently admin");
-            // Change to Member
-            adminService.updateUserRole(adminName, sampleUser, "MEMBER");
-            assertFalse(adminService.isAdmin(sampleUser), "SampleUser is now member");
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleUser);
-        }
-    }
-    @Test
-    void updateUserRoleFromMemberToMember() {
-        String sampleUser = "SampleUser";
-        try {
-            // Insert Member User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleUser, "empty", "MEMBER");
-            assertFalse(adminService.isAdmin(sampleUser), "SampleUser is currently member");
-            // Change to Member
-            adminService.updateUserRole(adminName, sampleUser, "MEMBER");
-            assertFalse(adminService.isAdmin(sampleUser), "SampleUser is still member");
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleUser);
-        }
+    void isAdminReturnFalseForNonExistingUser() {
+        assertFalse(adminService.isAdmin("NoExist"), "Non-existing user should return false");
     }
 
     @Test
+    void updateUserRoleFromMemberToMember() {
+        assertFalse(adminService.isAdmin(sampleMemberName), "SampleUser is currently member");
+        // Change to Member
+        adminService.updateUserRole(adminName, sampleMemberName, "MEMBER");
+        assertFalse(adminService.isAdmin(sampleMemberName), "SampleUser is still member");
+
+    }
+
+    @Test
+    void updateUserRoleFromAdminToAdmin() {
+        // Insert Member User
+        assertTrue(adminService.isAdmin(sampleAdminName), "SampleUser is currently admin");
+        // Change to Admin
+        adminService.updateUserRole(adminName, sampleAdminName, "ADMIN");
+        assertTrue(adminService.isAdmin(sampleAdminName), "SampleUser is still admin");
+    }
+    @Test
+    void updateUserRoleFromAdminToMember() {
+        // Check if currently Admin
+        assertTrue(adminService.isAdmin(sampleAdminName), "SampleUser is currently admin");
+        // Change to Member
+        adminService.updateUserRole(adminName, sampleAdminName, "MEMBER");
+        assertFalse(adminService.isAdmin(sampleAdminName), "SampleUser is now member");
+    }
+
+
+    @Test
     void updateUserRoleFromMemberToAdmin() {
-        String sampleUser = "SampleUser";
-        try {
-            // Insert Member User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleUser, "empty", "MEMBER");
-            assertFalse(adminService.isAdmin(sampleUser), "SampleUser is currently member");
-            // Change to Admin
-            adminService.updateUserRole(adminName, sampleUser, "ADMIN");
-            assertTrue(adminService.isAdmin(sampleUser), "SampleUser is now admin");
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleUser);
-        }
+        assertFalse(adminService.isAdmin(sampleMemberName), "SampleUser is currently member");
+        // Change to Admin
+        adminService.updateUserRole(adminName, sampleMemberName, "ADMIN");
+        assertTrue(adminService.isAdmin(sampleMemberName), "SampleUser is now admin");
     }
 
     @Test
     void updateUserRoleRejectNonAdmin(){
-        String sampleUser = "SampleUser";
-        try {
-            // Insert Member User
-            channelService.executeUpdate(insertUser, "Error inserting user", sampleUser, "empty", "MEMBER");
-            // Using sampleUser as requester since they're member
-            int rowAffected = adminService.updateUserRole("SampleUser", sampleUser, "MEMBER");
-            assertFalse(rowAffected > 0, "Update should not executed when not admin request");
-        } finally {
-            // Ensure clean up even with assertion fails
-            channelService.executeUpdate(deleteUser, "Error in deleting user", sampleUser);
-        }
+        int rowAffected = adminService.updateUserRole(sampleMemberName, sampleMemberName, "MEMBER");
+        assertFalse(rowAffected > 0, "Update should not executed when not admin request");
     }
 
     @Test
-    void deleteMessageExistingMessage() {
-        String sampleMessage = "SampleMessage";
-        String targetChannel = "General";
-        try {
-            // Insert a sample message
-            channelService.executeUpdate(insertMessage, "Error inserting message", adminName, sampleMessage, targetChannel);
-            // Getting the inserted sample message id
-            List<Map<String,Object>> results = channelService.executeQuery(getMessageId, adminName, sampleMessage, targetChannel);
-            // Deleting the message
-            assertTrue(adminService.deleteMessage((long)( (int) results.get(0).get("id"))), "Deleting message should return true");
+    void deleteMessageExistingMessage() {// Getting the inserted sample message id
+        List<Map<String,Object>> results = ServiceUtility.executeQuery(getMessageId, sampleMemberName, 1);
+        assertFalse(results.isEmpty(), "Message should exist");
+        // Deleting the message
+        assertTrue(adminService.deleteMessage(1L), "Deleting message should return true");
+        // Getting the result, should be empty
+        results = ServiceUtility.executeQuery(getMessageId, sampleMemberName, 1);
+        assertTrue(results.isEmpty(), "Message should have been deleted");
 
-            // Getting the result, should be empty
-            results = channelService.executeQuery(getMessageId, adminName, sampleMessage, targetChannel);
-            assertTrue(results.isEmpty(), "Message should have been deleted");
-        } finally {
-            // Just fail-safe in case function did not delete properly
-            channelService.executeUpdate(deleteMessage, "Error in deleting message", adminName, sampleMessage, targetChannel);
-        }
 
     }
     @Test
     void deleteMessageNonExistingMessage() {
-        String sampleMessage = "SampleMessage";
-        String targetChannel = "General";
-        try {
-            // Insert a sample message
-            channelService.executeUpdate(insertMessage, "Error inserting message", adminName, sampleMessage, targetChannel);
-            // Getting the inserted sample message id
-            List<Map<String,Object>> results = channelService.executeQuery(getMessageId, adminName, sampleMessage, targetChannel);
-            // Deleting the message
-            assertTrue(adminService.deleteMessage((long)( (int) results.get(0).get("id"))), "Deleting message should return true");
-            // Deleting the message again
-            assertFalse(adminService.deleteMessage((long)( (int) results.get(0).get("id"))), "Deleting message should return false");
-        } finally {
-            // Just fail-safe in case function did not delete properly
-            channelService.executeUpdate(deleteMessage, "Error in deleting message", adminName, sampleMessage, targetChannel);
-        }
-
+        List<Map<String,Object>> results = ServiceUtility.executeQuery(getMessageId, sampleMemberName, 1);
+        assertTrue(adminService.deleteMessage(1L), "Deleting message should return true");
+        // Deleting the message again
+        assertFalse(adminService.deleteMessage(1L), "Deleting message should return false");
     }
 
 }
