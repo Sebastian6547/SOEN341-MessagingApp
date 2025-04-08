ffeed71e5f352cca8fc1c89b5c1472dda510f853
Sebastian6547 <78222535+Sebastian6547@users.noreply.github.com>
2025-04-08 10:27:01 -0400

    Added unit tests for the channel controller

--- messagingApp-backend/src/test/java/com/messagingApp/messagingApp_backend/controllers/AdminControllerTest.java
+++ messagingApp-backend/src/test/java/com/messagingApp/messagingApp_backend/controllers/AdminControllerTest.java
@@ -1,4 +1,20 @@
 package com.messagingApp.messagingApp_backend.controllers;
 
+import com.messagingApp.messagingApp_backend.services.AdminService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = AdminController.class)
+@ActiveProfiles("test")
 public class AdminControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AdminService adminService;
 }
